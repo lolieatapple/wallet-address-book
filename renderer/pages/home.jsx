18cfdeced1b668541c89995959ccca8391cdf0f1
lolieatapple <lolieatapple@gmail.com>
2022-11-07 16:05:47 +0800

    update balance loading

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -14,34 +14,93 @@ import copy2Clipboard from 'copy-to-clipboard';
 import { MessageBox } from '../components/message';
 import { useState } from 'react';
 import sleep from 'ko-sleep';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
 const { ipcRenderer, shell } = require('electron');
 // const balanceApi = 'https://api.rabby.io/v1/user/total_balance?id=';
 const balanceApi = 'https://api.debank.com/user/total_balance?addr=';
+
+function TableLine(props) {
+  let v = props.v;
+  const [balance, setBalance] = useState('queued');
+  let update = props.update;
+  let setUpdate = props.setUpdate;
+  let i= props.i;
+  useEffect(()=>{
+    const func = async () => {
+      console.log('load balance', v.account);
+      try {
+        setBalance('loading');
+        let _balance = await fetch(balanceApi + v.account);
+        _balance = await _balance.json();
+        _balance = Number(Number(_balance.data.total_usd_value).toFixed(2));
+        setBalance('$'+_balance);
+      } catch (error) {
+        console.error(error.message);
+        setBalance('failed');
+      }
+    }
+
+    setTimeout(func, i * 5000);
+  }, [update]);
+  return (
+    <TableRow >
+      <TableCell>{JSON.parse(v.password).name} <Tooltip title="Modify" ><EditIcon sx={{ fontSize: '14px', position: 'relative', top: '1px', left: '2px', cursor: 'pointer' }} onClick={async () => {
+        let name = await ipcRenderer.invoke('prompt', { title: 'Modify Name', label: 'Name', value: JSON.parse(v.password).name, type: 'input' });
+        let json = JSON.parse(v.password);
+        json.name = name;
+        await ipcRenderer.invoke('setPk', { address: v.account, json: JSON.stringify(json) });
+        setUpdate(Date.now());
+      }} /></Tooltip></TableCell>
+      <TableCell>
+        <Stack spacing={1} direction="row" >
+          <div style={{ fontFamily: "Andale Mono" }}>{v.account}</div>
+          <Tooltip title="Copy Address" ><ContentCopyIcon sx={{ fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer' }} onClick={async () => {
+            if (copy2Clipboard(v.account)) {
+              setSuccessInfo("Address Copyed");
+            }
+          }} /></Tooltip>
+          <Tooltip title="Copy Private Key" ><VpnKeyIcon sx={{ fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer' }} onClick={async () => {
+            let pk = await ipcRenderer.invoke('getPk', v.account);
+            if (pk && copy2Clipboard(JSON.parse(pk).pk)) {
+              setSuccessInfo("Private Key Copyed");
+            }
+          }} /></Tooltip>
+          <Tooltip title="Delete Account" ><DeleteOutlineIcon sx={{ fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer' }} onClick={async () => {
+            await ipcRenderer.invoke('delPk', v.account);
+            setUpdate(Date.now());
+          }} /></Tooltip>
+        </Stack>
+
+      </TableCell>
+      <TableCell>{balance} <Tooltip title="Show In Debank" ><PublicIcon sx={{ fontSize: '14px', position: 'relative', top: '1px', left: '2px', cursor: 'pointer' }} onClick={async () => {
+        shell.openExternal('https://debank.com/profile/' + v.account);
+      }} /></Tooltip>
+      <Tooltip title="Refresh Balance" ><RefreshIcon sx={{ fontSize: '14px', position: 'relative', top: '1px', left: '2px', cursor: 'pointer' }} onClick={async () => {
+        try {
+          let _balance = await fetch(balanceApi + v.account);
+          _balance = await _balance.json();
+          _balance = Number(Number(_balance.data.total_usd_value).toFixed(2));
+          setBalance(_balance);
+        } catch (error) {
+          console.error(error.message);
+          setBalance('error');
+        }
+      }} /></Tooltip>
+      </TableCell>
+    </TableRow>
+  )
+}
 
 function Home() {
   const [update, setUpdate] = React.useState(0);
   const [addrs, setAddrs] = React.useState([]);
   const [successInfo, setSuccessInfo] = useState('');
   const [filter, setFilter] = useState('');
-  const [balances, setBalances] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     const func = async () => {
       let ret = await ipcRenderer.invoke('getAllPks');
       setAddrs(ret);
-      let _balances = [];
-      for (let i=0; i<ret.length; i++) {
-        try {
-          let _balance = await fetch(balanceApi + ret[i].account);
-          _balance = await _balance.json();
-          _balance = Number(Number(_balance.data.total_usd_value).toFixed(2));
-          _balances.push(_balance);
-          await sleep(200);
-        } catch (error) {
-          console.error(error.message);
-          _balances.push(0);
-        }
-      }
-      setBalances(_balances);
     }
 
     func();
@@ -51,29 +110,29 @@ function Home() {
       <Head>
         <title>Wallet Address Book</title>
       </Head>
-      <Stack spacing={1} style={{padding: 10}}>
+      <Stack spacing={1} style={{ padding: 10 }}>
         <Paper style={{ padding: 15, borderRadius: 15 }} elevation={4} >
           <Stack spacing={2} direction="row">
-            <TextField fullWidth size="small" label='Search / Filter' value={filter} onChange={e=>setFilter(e.target.value)} />
-            <Button size="small" variant='contained' sx={{textTransform:'none'}} onClick={async ()=>{
+            <TextField fullWidth size="small" label='Search / Filter' value={filter} onChange={e => setFilter(e.target.value)} />
+            <Button size="small" variant='contained' sx={{ textTransform: 'none' }} onClick={async () => {
               let wallet = ethers.Wallet.createRandom();
               let pk = wallet.privateKey;
               let addr = wallet.address;
               let name = 'Account_' + (new Date()).toISOString().split('.')[0];
-              let json = {name, pk};
-              await ipcRenderer.invoke('setPk', {address: addr, json: JSON.stringify(json)});
+              let json = { name, pk };
+              await ipcRenderer.invoke('setPk', { address: addr, json: JSON.stringify(json) });
               setUpdate(Date.now());
             }} >Create</Button>
-            <Button size="small" variant='contained' sx={{textTransform:'none'}} onClick={async ()=>{
-              let pk = await ipcRenderer.invoke('prompt', {title: 'Import Private Key', label: 'Private Key', value: '', type: 'input'});
+            <Button size="small" variant='contained' sx={{ textTransform: 'none' }} onClick={async () => {
+              let pk = await ipcRenderer.invoke('prompt', { title: 'Import Private Key', label: 'Private Key', value: '', type: 'input' });
               let wallet = new ethers.Wallet(pk);
               let addr = wallet.address;
               let name = 'Import_' + (new Date()).toISOString().split('.')[0];
-              let json = {name, pk};
-              await ipcRenderer.invoke('setPk', {address: addr, json: JSON.stringify(json)});
+              let json = { name, pk };
+              await ipcRenderer.invoke('setPk', { address: addr, json: JSON.stringify(json) });
               setUpdate(Date.now());
             }} >Import</Button>
-            <DarkModeButton style={{fontSize: '36px'}} />
+            <DarkModeButton style={{ fontSize: '36px' }} />
           </Stack>
         </Paper>
         <Paper style={{ padding: 15, borderRadius: 15 }} elevation={4} >
@@ -87,43 +146,10 @@ function Home() {
             </TableHead>
             <TableBody>
               {
-                addrs.filter(v=>JSON.parse(v.password).name.toLowerCase().includes(filter.toLowerCase()) || v.account.toLowerCase().includes(filter.toLowerCase())).map((v,i)=>{
-                  return <TableRow key={v.account} >
-                  <TableCell>{JSON.parse(v.password).name} <Tooltip title="Modify" ><EditIcon sx={{fontSize: '14px', position: 'relative', top: '1px', left: '2px', cursor: 'pointer'}} onClick={async ()=>{
-                    let name = await ipcRenderer.invoke('prompt', {title: 'Modify Name', label: 'Name', value: JSON.parse(v.password).name, type: 'input'});
-                    let json = JSON.parse(v.password);
-                    json.name = name;
-                    await ipcRenderer.invoke('setPk', {address: v.account, json: JSON.stringify(json)});
-                    setUpdate(Date.now());
-                  }} /></Tooltip></TableCell>
-                  <TableCell>
-                    <Stack spacing={1} direction="row" >
-                      <div style={{fontFamily:"Andale Mono"}}>{v.account}</div>
-                      <Tooltip title="Copy Address" ><ContentCopyIcon sx={{fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer'}}  onClick={async ()=>{
-                        if (copy2Clipboard(v.account)) {
-                          setSuccessInfo("Address Copyed");
-                        }
-                      }} /></Tooltip>
-                      <Tooltip title="Copy Private Key" ><VpnKeyIcon sx={{fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer'}}  onClick={async ()=>{
-                        let pk = await ipcRenderer.invoke('getPk', v.account);
-                        if (pk && copy2Clipboard(JSON.parse(pk).pk)) {
-                          setSuccessInfo("Private Key Copyed");
-                        }
-                      }} /></Tooltip>
-                      <Tooltip title="Delete Account" ><DeleteOutlineIcon sx={{fontSize: '14px', position: 'relative', top: '2px', left: '4px', cursor: 'pointer'}} onClick={async ()=>{
-                        await ipcRenderer.invoke('delPk', v.account);
-                        setUpdate(Date.now());
-                      }} /></Tooltip>
-                    </Stack>
-                    
-                  </TableCell>
-                  <TableCell>{balances[i] !== undefined ? '$'+balances[i]:'loading'} <Tooltip title="Show In Debank" ><PublicIcon sx={{fontSize: '14px', position: 'relative', top: '1px', left: '2px', cursor: 'pointer'}} onClick={async ()=>{
-                    shell.openExternal('https://debank.com/profile/' + v.account);
-                  }} /></Tooltip></TableCell>
-                </TableRow>
+                addrs.filter(v => JSON.parse(v.password).name.toLowerCase().includes(filter.toLowerCase()) || v.account.toLowerCase().includes(filter.toLowerCase())).map((v, i) => {
+                  return <TableLine key={v.account} v={v} update={update} setUpdate={setUpdate} i={i} />
                 })
               }
-              
             </TableBody>
           </Table>
         </Paper>
